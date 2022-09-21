83ca699fa15e52ce00bbfb26a17c74a4cbc1a08b
MunFahim <muntasirf362@gmail.com>
2022-09-20 20:04:06 -0400

    fixing

--- ignore/config.js
+++ ignore/config.js
@@ -1,4 +1,4 @@
 var config = {
-        weather_key: '4b4f1bcfac31ca37cf67b7426fc9427a',
-        facts_key: 'yOHtg6lrN8DqCjiT23nZ4g==FMb1FfVYL0dtzkS4',
+        weather_key: '',
+        facts_key: '',
 };
